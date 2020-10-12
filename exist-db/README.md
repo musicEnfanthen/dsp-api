be1aa793d2d75df0f702f3d4f28812911e3f2da1
BalduinLandolt <balduin.landolt@hotmail.com>
2020-10-12 20:00:29 +0200

    Feature (exist-db): add client code checking if the database is running

--- exist-db/README.md
+++ exist-db/README.md
@@ -14,10 +14,14 @@ Run eXist-db:
 ```shell
 docker run --rm -it  -p 8080:8080/tcp -p 8443:8443/tcp existdb/existdb:release
 ```
-First time (or when something changed): Add `knora-exist` app to your exist instance:  
+Each time the database is started: Add `knora-exist` app to your exist instance:  
 Open `localhost:/8080` in your browser. Log in with `user="admin"`, `password=""`, then click "Package manager" on the left. Install `knora-exist-0.1.xar` (in this folder) with "upload"-button or drag'n'drop.
 
 To check if eXist-db and knora-exist are up and running, open `http://localhost:8080/exist/apps/knora-exist/modules/test.xql` in your browser.  
 This may ask you for credentials: Again `user="admin"` and `password=""`.
 
+Note: If anything in the app is changed, it must be exported to a new .xar again.
 
+## Requirements
+
+* requests (`pip3 install requests`)
---
+++ exist-db/client.py
@@ -0,0 +1,43 @@
+import os
+import requests
+
+# variables
+exist_server = 'http://localhost:8080/exist'
+app_path = 'apps/knora-exist'
+
+# turn off proxy for localhost
+os.environ['NO_PROXY'] = '127.0.0.1'
+
+def run():
+    """
+    Run client requests
+    """
+    print('Client starting')
+    print(f'eXist-db status: {db_status()}')
+    print(f'App "knora-exist" status: {app_status()}')
+
+def app_status():
+    """
+    Tests if the `knora-exist` App is up and running
+    """
+    url = '/'.join((exist_server, app_path, 'test.xql'))
+    try:
+        r = requests.get(url)
+        return r.status_code
+    except Exception as e:
+        return 'app not found'
+
+def db_status():
+    """
+    Tests if exist-db is up and running.
+    """
+    stat_url = exist_server + '/status'
+    # print(stat_url)
+    try:
+        r = requests.get(stat_url, auth=('admin', ''))
+        return r.status_code
+    except Exception as e:
+        return 'Could not connect to database.'
+
+if __name__ == "__main__":
+    run()
